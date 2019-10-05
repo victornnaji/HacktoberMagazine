f23d5dbdc604d3d02061fc44fbebb0cc5da636ff
Nnaji Victor <37231697+Nnaji-Victor@users.noreply.github.com>
2019-10-05 20:30:29 +0100

    added a comma to your array item

--- script/index.js
+++ script/index.js
@@ -139,15 +139,14 @@ twitter: ""
     title: "Software Developer",
     facebook: "Chigozie Asadu",
     twitter: "Chigorgor"
-    }
-
+},
 {
     decadev: "Anushka Narula",
     github_username: "anushkanarula",
     title: "Computer Science Engineering Undergrad",
     facebook: "narula.anushka",
     twitter: "AnushkaNarula1"
-    }
+}
 
 ];
 
